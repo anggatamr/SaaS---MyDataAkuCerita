c88b7f373f26942de12054e7b9896315c2579b1c
anggatamr <anggatamara40@gmail.com>
2026-03-13 03:17:14 +0700

    Siap untuk deploy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    // @ts-expect-error Next.js 15 dev origins issue
+    allowedDevOrigins: ["*", "localhost:3000"],
+  },
   reactStrictMode: true,
   poweredByHeader: false,
 };
